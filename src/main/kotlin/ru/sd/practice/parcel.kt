b636e91c69dbbdf660223d8fd91d64101237b921
maxim <max-zu99@mail.ru>
2020-04-29 16:38:08 +0300

    TODO Add serialization

--- src/main/kotlin/ru/sd/practice/parcel.kt
+++ src/main/kotlin/ru/sd/practice/parcel.kt
@@ -2,4 +2,14 @@ package ru.sd.practice
 
 data class User(val name: String)
 
-data class Parcel(val time: Long, val user: User, val text: String)
+data class Parcel(val time: Long, val user: User, val text: String) {
+    fun toBytes(): ByteArray {
+        return ByteArray(2)
+    }
+
+    companion object {
+        fun fromBytes(bytes: ByteArray): Parcel {
+            return Parcel(0, User(""), "")
+        }
+    }
+}
